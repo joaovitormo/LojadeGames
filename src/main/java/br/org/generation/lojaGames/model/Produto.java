4864bffcc091e4ab78bed6d473d82a1fb31355de
joaovitormo <vitormendes388@gmail.com>
2021-06-30 21:13:27 -0300

    crud de uma loja de games

---
+++ src/main/java/br/org/generation/lojaGames/model/Produto.java
@@ -0,0 +1,141 @@
+package br.org.generation.lojaGames.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=5, max=100)
+	private String nome;
+	
+	@NotNull
+	@Size(min=5, max=100)
+	private String descricao;
+	
+	@NotNull
+	@Size(min=2, max=100)
+	private String faixaEtaria;
+	
+	@NotNull
+	@Size(min=1, max=100)
+	private String plataforma;
+	
+	@NotNull
+	@Size(min=5, max=100)
+	private String desenvolvedora;
+	
+	@NotNull
+	private double preco;
+	
+
+
+	@NotNull
+	private double quantidade;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getFaixaEtaria() {
+		return faixaEtaria;
+	}
+
+	public void setFaixaEtaria(String faixaEtaria) {
+		this.faixaEtaria = faixaEtaria;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public String getDesenvolvedora() {
+		return desenvolvedora;
+	}
+
+	public void setDesenvolvedora(String desenvolvedora) {
+		this.desenvolvedora = desenvolvedora;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(double quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
---
+++ src/main/java/br/org/generation/lojaGames/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.org.generation.lojaGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.lojaGames.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository <Produto, Long>{
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+}
